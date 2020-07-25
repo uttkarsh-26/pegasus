eb9cf142e4322a67404d98237eb09252380800e5
uttkarsh-26 <uttkarsh.26@gmail.com>
2020-07-25 08:43:33 +0530

    Update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit0ffc3ba718dff8b9500be6039fd2864c
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
